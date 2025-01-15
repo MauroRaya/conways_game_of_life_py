581df018db45be367d0aea0c022affa14b823e64
MauroRaya <maurorayabr@gmail.com>
2025-01-15 09:31:44 -0300

    fix: qol improvements

--- main.py
+++ main.py
@@ -1,47 +1,42 @@
 import pygame
 import sys
-import time
 
 pygame.init()
 pygame.display.set_caption('Conway\'s Game of Life')
 
+# Colors
 BACKGROUND_COLOR = (100, 100, 100) # Black
 GRID_COLOR       = (255, 255, 255) # White
-POPULATION_COLOR = (30, 30, 255)   # Blue
+ALIVE_COLOR      = (30, 30, 255)   # Blue
 
-ROWS = 15
-CURRENT_GENERATION = [[BACKGROUND_COLOR for _ in range(ROWS)] for _ in range(ROWS)]
-SIMULATION_RUNNING = False
+# Grid settings
+GRID_SIZE          = 15
+current_grid       = [[BACKGROUND_COLOR for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+simulation_running = False
 
-WIDTH  = 600
-HEIGHT = 600
-
-CELL_WIDTH  = WIDTH  / ROWS
-CELL_HEIGHT = HEIGHT / ROWS
-
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-
-
-def draw_grid(surface, rows, cell_width, cell_height):
-    for row in range(rows):
-        for col in range(rows):
-            rect = (row * cell_width, col * cell_height, cell_width, cell_height)
-            pygame.draw.rect(surface, CURRENT_GENERATION[row][col], rect) # Draws the color of the cell
-            pygame.draw.rect(surface, GRID_COLOR, rect, 1)                # Draws the border
+# Screen settings
+WIDTH       = 600
+HEIGHT      = 600
+CELL_WIDTH  = WIDTH  / GRID_SIZE
+CELL_HEIGHT = HEIGHT / GRID_SIZE
+screen      = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
-# Mouse click = new cell in the grid
-def handle_mouse_click(pos):
+def draw_grid(surface):
+    for row in range(GRID_SIZE):
+        for col in range(GRID_SIZE):
+            rect = (row * CELL_WIDTH, col * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
+            pygame.draw.rect(surface, current_grid[row][col], rect) # Cell color
+            pygame.draw.rect(surface, GRID_COLOR, rect, 1)          # Grid border
+
+
+def toggle_cell_state(pos):
     x, y = pos
 
     row = int(x // CELL_WIDTH)
     col = int(y // CELL_HEIGHT)
 
-    CURRENT_GENERATION[row][col] = POPULATION_COLOR if CURRENT_GENERATION[row][col] == BACKGROUND_COLOR else BACKGROUND_COLOR
-
-
-def is_cell_filled(grid_cell_value: list[list[tuple]]):
-    return grid_cell_value != BACKGROUND_COLOR
+    current_grid[row][col] = ALIVE_COLOR if current_grid[row][col] == BACKGROUND_COLOR else BACKGROUND_COLOR
 
 
 def count_neighbors(row, col) -> int:
@@ -53,60 +48,54 @@ def count_neighbors(row, col) -> int:
                 continue
             r, c = row + i, col + j
             
-            if 0 <= r < ROWS and 0 <= c < ROWS and CURRENT_GENERATION[r][c] == POPULATION_COLOR:
+            if 0 <= r < GRID_SIZE and 0 <= c < GRID_SIZE and current_grid[r][c] == ALIVE_COLOR:
                 count += 1
     
     return count
 
 
-def handle_next_generation():
-    global CURRENT_GENERATION
-    NEXT_GENERATION = CURRENT_GENERATION
+def update_generation():
+    global current_grid
+    next_grid = [[BACKGROUND_COLOR for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 
-    for row in range(ROWS):
-        for col in range(ROWS):
-            neighbor_count = count_neighbors(row, col)
+    for row in range(GRID_SIZE):
+        for col in range(GRID_SIZE):
+            neighbors = count_neighbors(row, col)
 
-            if neighbor_count < 2 or neighbor_count > 3:
-                NEXT_GENERATION[row][col] = BACKGROUND_COLOR
-
-            if neighbor_count == 3 and CURRENT_GENERATION[row][col] == BACKGROUND_COLOR:
-                NEXT_GENERATION[row][col] = POPULATION_COLOR
+            if neighbors == 3 or (neighbors == 2 and current_grid[row][col] == ALIVE_COLOR):
+                next_grid[row][col] = ALIVE_COLOR
+            else:
+                next_grid[row][col] = BACKGROUND_COLOR
     
-    CURRENT_GENERATION = NEXT_GENERATION
+    current_grid = next_grid
             
 
-
 def main():
-    global SIMULATION_RUNNING
+    global simulation_running
     clock = pygame.time.Clock()
 
     while True:
-        SCREEN.fill(BACKGROUND_COLOR)
-        draw_grid(SCREEN, ROWS, CELL_WIDTH, CELL_HEIGHT)
+        screen.fill(BACKGROUND_COLOR)
+        draw_grid(screen)
 
-        if SIMULATION_RUNNING:
-            handle_next_generation()
+        if simulation_running:
+            update_generation()
 
         for event in pygame.event.get():
-            # Prevents window from crashing... no clue why
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
                 
-            # Create population cells on the grid cells
             if event.type == pygame.MOUSEBUTTONDOWN:
-                handle_mouse_click(event.pos)
+                toggle_cell_state(event.pos)
 
-            # Detect key to toggle simulation
             if event.type == pygame.KEYDOWN:
                 key_pressed = pygame.key.get_pressed()
-
                 if key_pressed[pygame.K_0]:
-                    SIMULATION_RUNNING = not SIMULATION_RUNNING
+                    simulation_running = not simulation_running
 
         pygame.display.update()
-        clock.tick(10)
+        clock.tick(8)
 
 
 if __name__ == '__main__':
